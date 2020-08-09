095f04ecfe9a8fb1edc3a48b8c2f6d38981e2851
audeta35 <audeta35@gmail.com>
2020-08-10 04:53:20 +0700

    quest-num-0: separate function from main func & add unit testing 1, 2 and 3

--- quest-num-0/answer0.go
+++ quest-num-0/answer0.go
@@ -9,20 +9,11 @@ var number2 = 31
 // set variable total palindrome
 var totalPalindrome int
 
-func main() {
-
-	// set input value for number1
-	fmt.Print("input number1: \n")
-	fmt.Scan(&number1)
-
-	// set input value for number2
-	fmt.Print("input number2: \n")
-	fmt.Scan(&number2)
-
+func isPalindrome(a, b int) (res int) {
 	// check if first number is < than second number
-	if number1 < number2 {
+	if a < b {
 		// looping numbers based on user input
-		for i := number1; i < number2; i++ {
+		for i := a; i < b; i++ {
 			// set value for oriNumber and revNumber
 			oriNumber := i
 			revNumber := 0
@@ -41,10 +32,28 @@ func main() {
 			}
 		}
 
-		// showing total palindrome number between number1 and number2
-		fmt.Println(totalPalindrome)
-	} else {
-		fmt.Printf("cannot process palindrome numbers between %d and %d", number1, number2)
-		fmt.Print("\n")
-	}
+		res = totalPalindrome
+
+	} // else {
+	// 	fmt.Printf("cannot process palindrome numbers between %d and %d", a, b)
+	// 	fmt.Print("\n")
+	// }
+
+	return res
+}
+
+func main() {
+
+	// set input value for number1
+	fmt.Print("input number1: \n")
+	fmt.Scan(&number1)
+
+	// set input value for number2
+	fmt.Print("input number2: \n")
+	fmt.Scan(&number2)
+
+	// showing total palindrome number between a and b
+	fmt.Printf("%v < %v", number1, number2)
+	fmt.Println(" ")
+	fmt.Println("total palindrome: ", isPalindrome(number1, number2))
 }
--- quest-num-0/answer0_test.go
+++ quest-num-0/answer0_test.go
@@ -5,50 +5,35 @@ import (
 	"testing"
 )
 
-// testing for main function answer0.go
-func TestMain(t *testing.T) {
+func TestInput1(t *testing.T) {
+	input := isPalindrome(1, 10)
 
-	if number1 == 1 && number2 == 10 {
-		// unit test 1
-		main()
-		if totalPalindrome == 9 {
-			fmt.Println("unit test 1 with result :", totalPalindrome)
-		} else {
-			t.Logf("Got %d expect %d", totalPalindrome, 9)
-			t.Fail()
-		}
-	}
-
-	if number1 == 99 && number2 == 100 {
-		// unit test 2
-		main()
-		if totalPalindrome == 1 {
-			fmt.Println("unit test 2 with result :", totalPalindrome)
-		} else {
-			t.Logf("Got %d expect %d", totalPalindrome, 1)
-			t.Fail()
-		}
-	}
-
-	if number1 == 21 && number2 == 31 {
-		// unit test 3
-		main()
-		if totalPalindrome == 1 {
-			fmt.Println("unit test 3 with result :", totalPalindrome)
-		} else {
-			t.Logf("Got %d expect %d", totalPalindrome, 1)
-			t.Fail()
-		}
-	}
-
-	if number1 > number2 {
-		// other conditional
-		t.Logf("first number is : %d must be < than %d", number1, number2)
-		t.Fatal()
-	} else if number1 == number2 {
-		t.Log("first number cannot be == ", number2)
-		t.Fail()
+	if input == 9 {
+		fmt.Println("unit test 1 with result :", input)
 	} else {
-		// testing successfully!
+		t.Logf("Got %d expect %d", input, 9)
+		t.Fail()
+	}
+}
+
+func TestInput2(t *testing.T) {
+	input := isPalindrome(99, 100)
+
+	if input == 1 {
+		fmt.Println("unit test 2 with result :", input)
+	} else {
+		t.Logf("Got %d expect %d", input, 1)
+		t.Fail()
+	}
+}
+
+func TestInput3(t *testing.T) {
+	input1 := isPalindrome(21, 31)
+
+	if input1 == 1 {
+		fmt.Println("unit test 3 with result :", input1)
+	} else {
+		t.Logf("Got %d expect %d", input1, 1)
+		t.Fail()
 	}
 }
